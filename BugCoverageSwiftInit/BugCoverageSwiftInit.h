0f41a21f303e2d7df6a9b8fbab48d5618465a803
Paul Zabelin <pzabelin+git@gmail.com>
2018-06-01 13:59:38 -0700

    Initial Commit

---
+++ BugCoverageSwiftInit/BugCoverageSwiftInit.h
@@ -0,0 +1,19 @@
+//
+//  BugCoverageSwiftInit.h
+//  BugCoverageSwiftInit
+//
+//  Created by Paul Zabelin on 6/1/18.
+//  Copyright © 2018 Paul Zabelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BugCoverageSwiftInit.
+FOUNDATION_EXPORT double BugCoverageSwiftInitVersionNumber;
+
+//! Project version string for BugCoverageSwiftInit.
+FOUNDATION_EXPORT const unsigned char BugCoverageSwiftInitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BugCoverageSwiftInit/PublicHeader.h>
+
+
